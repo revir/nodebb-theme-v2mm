c9ea9204b1a708d72d2e443dfe6df83947daa416
barisusakli <barisusakli@gmail.com>
2014-03-24 17:52:51 -0400

    lavender changes

--- static/lib/lavender.js
+++ static/lib/lavender.js
@@ -43,15 +43,26 @@ $('document').ready(function() {
 			}
 		}
 
+		function isMobile() {
+			return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i).test(navigator.userAgent);
+		}
+
 		$(window).on('action:ajaxify.end', function(ev, data) {
 			var url = data.url;
 
 			if (url === '') {
-				if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+				if(!isMobile()) {
 					resize();
 				}
 			}
 		});
+
+		if (ajaxify.currentPage) {
+			if(!isMobile()) {
+				resize();
+			}
+		}
+
 
 		var div = $('<div class="panel resizer pointer"><div class="panel-body"><i class="fa fa-bars fa-2x"></i></div></div>');
 		div.css({
